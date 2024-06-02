c2a69ddd0ca47af275072fde0321862bd9fb3b7e
Esteban <es.alamos@duocuc.cl>
2024-06-02 19:01:53 -0400

    juegos exclusivos ps

---
+++ juegos.js
@@ -0,0 +1,63 @@
+
+
+const juegos = [
+    {
+        "id": "1",
+        "name": "Marvel's Spiderman 2",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1697557622-marvels-spider-man-2-ps5-pre-orden-0.jpg",
+        "consola": "PS5",
+        "precio": "30.000",
+        "cantidad": 60,
+        "descripcion": "Es una ambiciosa secuela que promete mejorar en todos los aspectos sobre sus predecesores, ofreciendo una experiencia de superhéroe inmersiva y emocionante para los fanáticos de Spider-Man y los juegos de acción y aventura."
+    },   
+    {
+        "id": "2",
+        "name": "Street Fighter VI",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1685745015-street-fighter-vi-ps5-0.jpg",
+        "consola": "PS5",
+        "precio": "25.000",
+        "cantidad": 20,
+        "descripcion": "Es una evolución emocionante de la franquicia que combina elementos clásicos con innovaciones modernas, prometiendo satisfacer tanto a los veteranos de la serie como a los nuevos jugadores con su jugabilidad refinada, gráficos mejorados y una experiencia en línea robusta."
+    },
+    {
+        "id": "3",
+        "name": "Among Us",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1639589329-among-us-ps5.jpg",
+        "consola": "PS5",
+        "precio": "2.000",
+        "cantidad": 17,
+        "descripcion": "Es un juego multijugador social de deducción con una jugabilidad simple pero adictiva, que ha dejado una marca significativa en la cultura de los videojuegos y el entretenimiento en línea."
+    },
+    {
+        "id": "4",
+        "name": "Dark Souls 3",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1618956755-dark-souls-iii-ps5.jpg",
+        "consola": "PS5",
+        "precio": "15.000",
+        "cantidad": 29,
+        "descripcion": "Es un juego de rol de acción que combina un combate desafiante, una rica narrativa y un mundo detalladamente diseñado, proporcionando una experiencia profunda y gratificante."
+    },
+    {
+        "id": "5",
+        "name": "Uncharted 4 ",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1618959196-uncharted-4-el-desenlace-del-ladron-ps5.jpg",
+        "consola": "PS5",
+        "precio": "12.000",
+        "cantidad": 44,
+        "descripcion": "Ofrece una combinación de narrativa emocionante, jugabilidad variada y gráficos sobresalientes, consolidándose como una de las mejores experiencias de acción y aventura "
+    },
+    {
+        "id": "6",
+        "name": "Until Dawn",
+        "img": "https://juegosdigitaleschile.com/files/images/productos/1715790830-until-dawn-exclusivo-ps5-pre-orden-0.webp",
+        "consola": "PS5",
+        "precio": "18.000",
+        "cantidad": 40,
+        "descripcion": "Unti Dawn ofrece una experiencia de terror interactiva donde tus decisiones afectan directamente la vida o la muerte de los personajes. Si eres fanático de los juegos de terror y las historias inmersivas, definitivamente deberías darle una oportunidad."
+    },
+
+];
+
+module.exports = {
+    juegos
+} 
